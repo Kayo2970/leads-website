47df20172f5229e5ca940923c0699b499893b4d5
Kayomarz Pavri <kayo@Kayomarzs-MacBook-Air.local>
2026-03-24 20:24:06 +0530

    Create creative animated geometric shapes for preloader and dynamic hero background with animated gradients and patterns

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -23,6 +23,19 @@ export default function Preloader() {
 
   return (
     <div className={`preloader ${hidden ? 'hide' : ''}`} id="preloader">
+      {/* Animated Geometric Shapes */}
+      <div className="geometric-shapes">
+        <div className="shape shape-1" />
+        <div className="shape shape-2" />
+        <div className="shape shape-3" />
+        <div className="shape shape-4" />
+        <div className="shape shape-5" />
+        <div className="shape shape-6" />
+        <div className="animated-line line-1" />
+        <div className="animated-line line-2" />
+        <div className="animated-line line-3" />
+      </div>
+
       <div className="preloader-container">
         <div className="logo-spinner">
           <Image
